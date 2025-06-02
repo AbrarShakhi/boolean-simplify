b9a924334edb4390054132f841d9edc4815d85d6
AbrarShakhi <114344002+AbrarShakhi@users.noreply.github.com>
2025-06-02 23:44:22 +0600

    clear

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}></main>
+      <h1> Karnaugh Map</h1>
       <footer className={styles.footer}></footer>
     </div>
   );
